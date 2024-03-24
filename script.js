a2cc9578e79facd0ef531a9a53d63d7f9ebb5eb8
Morgana Martins <morganadsm@outlook.com>
2024-03-24 18:01:17 -0300

    mudei tudo

--- script.js
+++ script.js
@@ -1,54 +1,18 @@
-function agendar() {
-  var data = document.getElementById("datePicker").value;
-  var hora = document.getElementById("timePicker").value;
+document.getElementById("appointment-form").addEventListener("submit", function(event) {
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+  var telefone = document.getElementById("telefone").value;
+  var servico = document.getElementById("servico").value;
+  var data = document.getElementById("data").value;
+  var hora = document.getElementById("hora").value;
+  var errorMessage = "";
 
-  if (data && hora) {
-    var mensagem = "Horário agendado para " + data + " às " + hora + ".";
-    document.getElementById("mensagem").innerText = mensagem;
-  } else {
-    document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
+  if (nome === "" || email === "" || telefone === "" || servico === "" || data === "" || hora === "") {
+      errorMessage = "Todos os campos são obrigatórios.";
   }
-}
 
-function login() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-
-  if (username === "admin" && password === "123456") {
-    document.getElementById("loginForm").classList.add("hidden");
-    document.getElementById("agendaForm").classList.remove("hidden");
-  } else {
-    alert("Nome de usuário ou senha incorretos");
+  if (errorMessage !== "") {
+      document.getElementById("error-message").innerText = errorMessage;
+      event.preventDefault();
   }
-}
-
-function agendar() {
-  var data = document.getElementById("datePicker").value;
-  var hora = document.getElementById("timePicker").value;
-
-  if (data && hora) {
-    var mensagem = "Horário agendado para " + data + " às " + hora + ".";
-    document.getElementById("mensagem").innerText = mensagem;
-  } else {
-    document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
-  }
-}
-
-function cadastrar() {
-var cadastroUsername = document.getElementById("cadastroUsername").value;
-var cadastroPassword = document.getElementById("cadastroPassword").value;
-var confirmarPassword = document.getElementById("confirmarPassword").value;
-
-if (cadastroUsername && cadastroPassword && confirmarPassword) {
-  if (cadastroPassword === confirmarPassword) {
-    // Se as senhas coincidem, você pode proceder com o cadastro
-    alert("Cadastro realizado com sucesso!");
-    // Aqui você pode adicionar lógica para salvar os detalhes do usuário, como em um banco de dados
-  } else {
-    alert("As senhas não coincidem. Por favor, tente novamente.");
-  }
-} else {
-  alert("Por favor, preencha todos os campos.");
-}
-}
+});
