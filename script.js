313c1f64ddb7ab59cc32647643f1157740b38e56
Morgana Martins <morganadsm@outlook.com>
2024-03-24 17:17:48 -0300

    To perdida

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 function agendar() {
-    var data = document.getElementById("datePicker").value;
-    var hora = document.getElementById("timePicker").value;
-  
-    if (data && hora) {
-      var mensagem = "Horário agendado para " + data + " às " + hora + ".";
-      document.getElementById("mensagem").innerText = mensagem;
-    } else {
-      document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
-    }
+  var data = document.getElementById("datePicker").value;
+  var hora = document.getElementById("timePicker").value;
+
+  if (data && hora) {
+    var mensagem = "Horário agendado para " + data + " às " + hora + ".";
+    document.getElementById("mensagem").innerText = mensagem;
+  } else {
+    document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
   }
+}
 
 function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-  
-    if (username === "admin" && password === "123456") {
-      document.getElementById("loginForm").classList.add("hidden");
-      document.getElementById("agendaForm").classList.remove("hidden");
-    } else {
-      alert("Nome de usuário ou senha incorretos");
-    }
-  }
-  
-  function agendar() {
-    var data = document.getElementById("datePicker").value;
-    var hora = document.getElementById("timePicker").value;
-  
-    if (data && hora) {
-      var mensagem = "Horário agendado para " + data + " às " + hora + ".";
-      document.getElementById("mensagem").innerText = mensagem;
-    } else {
-      document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
-    }
-  }
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-  function cadastrar() {
-  var cadastroUsername = document.getElementById("cadastroUsername").value;
-  var cadastroPassword = document.getElementById("cadastroPassword").value;
-  var confirmarPassword = document.getElementById("confirmarPassword").value;
 
-  if (cadastroUsername && cadastroPassword && confirmarPassword) {
-    if (cadastroPassword === confirmarPassword) {
-      // Se as senhas coincidem, você pode proceder com o cadastro
-      alert("Cadastro realizado com sucesso!");
-      // Aqui você pode adicionar lógica para salvar os detalhes do usuário, como em um banco de dados
-    } else {
-      alert("As senhas não coincidem. Por favor, tente novamente.");
-    }
+  if (username === "admin" && password === "123456") {
+    document.getElementById("loginForm").classList.add("hidden");
+    document.getElementById("agendaForm").classList.remove("hidden");
   } else {
-    alert("Por favor, preencha todos os campos.");
+    alert("Nome de usuário ou senha incorretos");
   }
+}
+
+function agendar() {
+  var data = document.getElementById("datePicker").value;
+  var hora = document.getElementById("timePicker").value;
+
+  if (data && hora) {
+    var mensagem = "Horário agendado para " + data + " às " + hora + ".";
+    document.getElementById("mensagem").innerText = mensagem;
+  } else {
+    document.getElementById("mensagem").innerText = "Por favor, selecione uma data e hora.";
+  }
+}
+
+function cadastrar() {
+var cadastroUsername = document.getElementById("cadastroUsername").value;
+var cadastroPassword = document.getElementById("cadastroPassword").value;
+var confirmarPassword = document.getElementById("confirmarPassword").value;
+
+if (cadastroUsername && cadastroPassword && confirmarPassword) {
+  if (cadastroPassword === confirmarPassword) {
+    // Se as senhas coincidem, você pode proceder com o cadastro
+    alert("Cadastro realizado com sucesso!");
+    // Aqui você pode adicionar lógica para salvar os detalhes do usuário, como em um banco de dados
+  } else {
+    alert("As senhas não coincidem. Por favor, tente novamente.");
+  }
+} else {
+  alert("Por favor, preencha todos os campos.");
+}
 }
